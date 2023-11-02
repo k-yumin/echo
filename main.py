ce56178755890b33e68706ada0b347b86bf1d76d
k-yumin <kyumnkm@gmail.com>
2023-11-02 06:58:54 +0000

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
 import streamlit as st
 from gtts import gTTS
 
+nsf_hifigan = False
 
 def main():
     # title
@@ -10,27 +12,57 @@ def main():
     tab_titles = ['음성 생성', '모델 학습']
     voice_tab, model_tab = st.tabs(tab_titles)
 
+    # voice tab
     with voice_tab:
         st.header(tab_titles[0])
-        models = ('junseo', 'jimin')  # TODO
-        model_option = st.selectbox('모델을 선택해주세요.', models)
-        text = st.text_input('텍스트를 입력해주세요.')
 
+        # model select
+        model_list = os.listdir('diff-svc/checkpoints')
+        if 'nsf_hifigan' in model_list:
+            nsf_hifigan = True
+        
+        model_except = ['hubert', '0102_xiaoma_pe', 'nsf_hifigan', '0109_hifigan_bigpopcs_hop128']
+        model_list = [model for model in model_list if model not in model_except]
+        model_option = st.selectbox('모델을 선택해주세요.', model_list)
+
+        # text input
+        text = st.text_input('텍스트를 입력해주세요.')
+        
+        # infer button
         if st.button('음성 생성하기'):
-            st.write('음성 생성 중..')  # TODO
-            inferred_audio = open('diff-svc/results/test_output.wav', 'rb')
+            st.write('음성 생성 중..')
+            infer(text)
+
+            # get result
+            inferred_audio = open('diff-svc/results/test_output.wav', 'rb') # TODO
             st.audio(inferred_audio.read(), format='audio/wav')
             st.success('음성 생성 완료!')
 
+    # model tab
     with model_tab:
         st.header(tab_titles[1])
+
+        # model name input
+        model_name = st.text_input('모델 이름을 입력해주세요.')
+
+        # model upload
         uploader_msg = '학습을 위한 목소리 녹음 파일을 업로드해주세요. 총 분량이 1시간 이상이어야 합니다.'
         voice_files = st.file_uploader(uploader_msg, accept_multiple_files=True)
+
+        # train button
+        if st.button('모델 학습하기'):
+            st.write('모델 학습 중..')
+            train(model_name, voice_files)
 
 
 def infer(text):
     tts = gTTS(text, lang='ko')
-    tts.save('google_voice.wav')
+    tts.save('workspace/tts.wav')
+    # TODO
+
+
+def train(model_name, voice_files):
+    pass
     # TODO
 
 
