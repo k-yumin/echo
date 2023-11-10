43635d3caf78e07916a3092323a890fd9d118d12
k-yumin <kyumnkm@gmail.com>
2023-11-10 16:39:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-import os
-import shutil
-import subprocess
 import streamlit as st
+import numpy as np
+import soundfile as sf
+import shutil
+import io
+import subprocess
+import os
+from resemblyzer import preprocess_wav, VoiceEncoder
+from pathlib import Path
+from st_audiorec import st_audiorec
 from gtts import gTTS
 
 nsf_hifigan = False
@@ -12,14 +18,14 @@ def main():
     st.title('에코')
 
     # tabs
-    tab_titles = ['음성 생성', '모델 학습']
-    voice_tab, model_tab = st.tabs(tab_titles)
+    tab_titles = ['음성 생성', '모델 학습', '목소리 유사도 측정']
+    voice_tab, model_tab, test_tab = st.tabs(tab_titles)
 
-    # voice tab
+    # 음성 생성 탭
     with voice_tab:
         st.header(tab_titles[0])
 
-        # model select
+        # 모델 선택
         model_list = os.listdir('diff-svc/checkpoints')
         if 'nsf_hifigan' in model_list:
             nsf_hifigan = True
@@ -28,44 +34,76 @@ def main():
         model_list = [model for model in model_list if model not in model_except]
         model_option = st.selectbox('모델을 선택해주세요.', model_list)
 
-        # text input
+        # 텍스트 입력
         text = st.text_input('텍스트를 입력해주세요.')
         
-        # infer button
+        # 추론 시작 버튼
         if st.button('음성 생성하기'):
             st.write('음성 생성 중...')
             infer(model_option, text)
 
-            # get result
-            inferred_audio = open('diff-svc/results/speech.wav', 'rb') # TODO
+            # 결과 출력
+            inferred_audio = open('diff-svc/results/speech.wav', 'rb')
             st.audio(inferred_audio.read(), format='audio/wav')
             st.success('음성 생성 완료!')
 
-    # model tab
+    # 모델 학습 탭
     with model_tab:
         st.header(tab_titles[1])
 
-        # model name input
+        # 모델 이름 입력
         model_name = st.text_input('모델 이름을 입력해주세요.')
 
-        # model upload
+        # 모델 업로드
         uploader_msg = '학습을 위한 목소리 녹음 파일을 업로드해주세요. 총 분량이 1시간 이상이어야 합니다.'
         voice_files = st.file_uploader(uploader_msg, accept_multiple_files=True)
 
-        col1, col2 = st.columns([.21, 1])
-        
-        # train button
+        col1, col2 = st.columns([.25, 1])
+
+        # 학습 시작 버튼
         with col1:
             if st.button('모델 학습하기'):
                 st.write('모델 학습 중...')
                 train(model_name, voice_files)
-        # open tensorboard button
+        # TensorBoard 열기 버튼
         with col2:
             if st.button('TensorBoard 열기'):
-                st.success('포트 6006에서 TensorBoard가 성공적으로 열렸습니다!')
                 open_tensorboard(model_name)
 
+    # 목소리 유사도 측정 탭 (결과물 퀄리티를 확인하기 위함)
+    with test_tab:
 
+        st.write('자신의 목소리를 녹음하면, 최근 생성된 결과물과 목소리 유사도를 측정할 수 있습니다.')
+
+        try:
+            os.remove('workspace/my_voice.wav')
+        except FileNotFoundError:
+            pass
+        
+        wav_audio_data = st_audiorec()
+        
+        if wav_audio_data is not None:
+            f = open("workspace/my_voice.wav", "bx")
+            f.write(wav_audio_data)
+            f.close()
+        
+        # 목소리 유사도 측정 버튼
+        if st.button("목소리 유사도 측정하기"):
+            st.write('유사도 측정 중...')
+            similarity = 100 * compare_voices('workspace/my_voice.wav', 'diff-svc/results/speech.wav') 
+        
+            # 유사도 평가
+            if similarity > 70:
+                estimate = '동일 인물의 목소리일 가능성이 높습니다.'
+            elif similarity > 60:
+                estimate = '동일 인물의 목소리일 가능성이 있습니다.'
+            else:
+                estimate = '동일 인물의 목소리일 가능성이 낮습니다.'
+        
+            st.success(f"Similarity: {similarity:.2f}% ({estimate})")
+
+
+# 음성 생성 함수
 def infer(model_name, text):
     tts = gTTS(text, lang='ko')
     tts.save('diff-svc/raw/text.wav')
@@ -84,7 +122,7 @@ def infer(model_name, text):
     os.system('python infer_.py')
     os.chdir('..')
 
-
+# 모델 학습 함수
 def train(model_name, voice_files):
     presets = f"""raw_data_dir: diff-svc/preprocess_out/final
         binary_data_dir: diff-svc/data/binary/{model_name}
@@ -110,9 +148,30 @@ def train(model_name, voice_files):
     os.system(f'CUDA_VISIBLE_DEVICES=0 python run.py --config {config_path} --exp_name {model_name} --reset')
 
 
+# TensorBoard 열기 함수
 def open_tensorboard(model_name):
     subprocess.run(['tensorboard', '--load_fast=true', '--reload_interval=1', '--reload_multifile=true',
         f'--logdir=diff-svc/checkpoints/{model_name}/lightning_logs', '--port=6006'])
+
+
+# 목소리 유사도 측정 함수
+def compare_voices(file_path1, file_path2):
+    wav_path1 = Path(file_path1)
+    wav_path2 = Path(file_path2)
+
+    wav1 = preprocess_wav(wav_path1)
+    wav2 = preprocess_wav(wav_path2)
+
+    encoder = VoiceEncoder()
+
+    # 두 음성 파일을 벡터 형태로 변환하여 특징 추출
+    embedding1 = encoder.embed_utterance(wav1)
+    embedding2 = encoder.embed_utterance(wav2)
+
+    # 코사인 유사도(cosine similarity)를 사용하여 두 음성 간의 유사도 측정
+    similarity = np.dot(embedding1, embedding2) / (np.linalg.norm(embedding1) * np.linalg.norm(embedding2))
+
+    return similarity
 
 
 if __name__ == '__main__':
